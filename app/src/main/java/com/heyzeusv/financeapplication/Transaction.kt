7c1b6aa871d8b953031d08fae2006efa08a79858
HeyZeusV <valdesj419@gmail.com>
2019-09-24 19:05:07 -0700

    Finished base Transaction class that lists info that each transaction will hold. Finished rough layout draft for fragment that will show a Transaction.

---
+++ app/src/main/java/com/heyzeusv/financeapplication/Transaction.kt
@@ -0,0 +1,18 @@
+package com.heyzeusv.financeapplication
+
+import java.math.BigDecimal
+import java.util.*
+
+/*
+    Must be annotated with @Entity
+    if tableName not provided then class name is used as tableName
+*/
+data class Transaction(val id : Int = 0,
+                       var title : String = "",
+                       var date : Date = Date(),
+                       var total : BigDecimal = BigDecimal("0.0"),
+                       val itemList : Int = id,
+                       var memo : String = "",
+                       var category : String = "",
+                       var repeating : Boolean = false,
+                       var frequency : Int = 0)
