4a9b4b2bf918946ab26c528a71f596c6a30bafd3
HeyZeusV <valdesj419@gmail.com>
2021-01-18 16:30:25 -0800

    More set up for integration tests. HiltExt.kt/HiltTestActivity.kt/AndroidManifest.xml: Needed in order to be able to run Integration tests that use Hilt. Taken from architecture-samples repo: https://github.com/android/architecture-samples/tree/dev-hilt build.gradle: Removed plug-in that is no longer supported. Updated dependency.

---
+++ app/src/debug/java/com/heyzeusv/plutuswallet/HiltTestActivity.kt
@@ -0,0 +1,8 @@
+package com.heyzeusv.plutuswallet
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+// https://github.com/android/architecture-samples/blob/dev-hilt/app/src/debug/java/com/example/android/architecture/blueprints/todoapp/HiltTestActivity.kt
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
