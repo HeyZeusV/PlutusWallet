77401b57b0a5481b393aad07933c2cfe3bf4dead
HeyZeusV <valdesj419@gmail.com>
2021-03-08 15:08:02 -0800

    Started working on FilterFragment redesign, got into it and forgot to break this commit up.. CFLFragment.kt: Implemented new menu item, "Edit Filter". Click on this new menu item will cause FilterFragment to appear/disappear. ic_baseline_filter_24.xml: New icon to be used by filter menu item. ic_baseline_save_24.xml: Previous save icon was picked from round icon set, so decided to change icon to match same icon set as other icons. ic_baseline_settings_24.xml: Renamed since it comes from same icon set as others and to tell icon size as well. fragment_cfl.xml: Changes to Chart/Filter Fragment constraints so that they match width of parent. FilterFragment now starts off screen. Added anchor to be used by FilterFragment when it is shown. fragment_chart.xml/fragment_filter.xml: Change to CardView margins due to not having a shared side anymore. cfl_menu.xml: Added new filter menu item. navigation_menu.xml: Updated icon name. transaction_menu.xml: Updated icon name. strings.xml: Added string for new filter menu and updated name for other CFL menu item.

--- app/src/main/java/com/heyzeusv/plutuswallet/ui/cfl/CFLFragment.kt
+++ app/src/main/java/com/heyzeusv/plutuswallet/ui/cfl/CFLFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import androidx.transition.TransitionManager
 import com.heyzeusv.plutuswallet.R
 import com.heyzeusv.plutuswallet.databinding.FragmentCflBinding
 
@@ -25,6 +27,8 @@ class CFLFragment : Fragment() {
 
     // shared ViewModels
     private val cflVM: CFLViewModel by activityViewModels()
+
+    var filterShown = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,16 +48,37 @@ class CFLFragment : Fragment() {
 
         // handles menu selection
         binding.cflTopBar.setOnMenuItemClickListener { item: MenuItem ->
-            if (item.itemId == R.id.cfl_new_tran) {
-                // creates action with parameters
-                val action: NavDirections =
-                    CFLFragmentDirections.actionTransaction()
-                findNavController().navigate(action)
-                // scroll back to top of list
-                cflVM.filterChanged = true
-                true
-            } else {
-                false
+            when (item.itemId) {
+                R.id.cfl_new_tran -> {
+                    // creates action with parameters
+                    val action: NavDirections =
+                        CFLFragmentDirections.actionTransaction()
+                    findNavController().navigate(action)
+                    // scroll back to top of list
+                    cflVM.filterChanged = true
+                    true
+                }
+                R.id.cfl_edit_filter -> {
+                    val constraints = ConstraintSet()
+                    constraints.clone(binding.cflConstraint)
+                    filterShown = if (filterShown) {
+                        constraints.connect(
+                            R.id.fragment_filter_container, ConstraintSet.BOTTOM,
+                            R.id.cfl_constraint, ConstraintSet.TOP, 0
+                        )
+                        !filterShown
+                    } else {
+                        constraints.connect(
+                            R.id.fragment_filter_container, ConstraintSet.BOTTOM,
+                            R.id.cfl_filter_anchor, ConstraintSet.TOP, 0
+                        )
+                        !filterShown
+                    }
+                    constraints.applyTo(binding.cflConstraint)
+                    TransitionManager.beginDelayedTransition(binding.cflConstraint)
+                    true
+                }
+                else -> false
             }
         }
 
