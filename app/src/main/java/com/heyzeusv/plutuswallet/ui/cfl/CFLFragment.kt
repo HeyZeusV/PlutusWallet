7deab8e043d558f4becc4d6298ddd6e93c2a6307
HeyZeusV <valdesj419@gmail.com>
2021-03-09 14:17:59 -0800

    Added mask to darken background when filter is open. CFLFragment.kt: Created function that handles filter opening and closing. New function also darkens/lightens background mask when filter is open/closed. Clicking on this new mask when filter is open will close filter. fragment_cfl.xml: Added side margins to FilterFragment, might change them later. Added TextView to be used as a mask when filter is open. Formatted using Ctrl+Alt+L.

--- app/src/main/java/com/heyzeusv/plutuswallet/ui/cfl/CFLFragment.kt
+++ app/src/main/java/com/heyzeusv/plutuswallet/ui/cfl/CFLFragment.kt
@@ -1,10 +1,13 @@
 package com.heyzeusv.plutuswallet.ui.cfl
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
@@ -13,6 +16,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
 import com.heyzeusv.plutuswallet.R
 import com.heyzeusv.plutuswallet.databinding.FragmentCflBinding
@@ -28,7 +32,10 @@ class CFLFragment : Fragment() {
     // shared ViewModels
     private val cflVM: CFLViewModel by activityViewModels()
 
-    var filterShown = false
+    // constraint set used by filter animation
+    private val constraints = ConstraintSet()
+
+    private var filterShown = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -46,6 +53,13 @@ class CFLFragment : Fragment() {
                 .openDrawer(GravityCompat.START)
         }
 
+        constraints.clone(binding.cflConstraint)
+
+        // clicking outside the filter area will close it
+        binding.cflFilterMask.setOnClickListener {
+            filterInteraction()
+        }
+
         // handles menu selection
         binding.cflTopBar.setOnMenuItemClickListener { item: MenuItem ->
             when (item.itemId) {
@@ -59,23 +73,11 @@ class CFLFragment : Fragment() {
                     true
                 }
                 R.id.cfl_edit_filter -> {
-                    val constraints = ConstraintSet()
-                    constraints.clone(binding.cflConstraint)
-                    filterShown = if (filterShown) {
-                        constraints.connect(
-                            R.id.fragment_filter_container, ConstraintSet.BOTTOM,
-                            R.id.cfl_constraint, ConstraintSet.TOP, 0
-                        )
-                        !filterShown
+                    if (filterShown) {
+                        filterInteraction()
                     } else {
-                        constraints.connect(
-                            R.id.fragment_filter_container, ConstraintSet.BOTTOM,
-                            R.id.cfl_filter_anchor, ConstraintSet.TOP, 0
-                        )
-                        !filterShown
+                        filterInteraction()
                     }
-                    constraints.applyTo(binding.cflConstraint)
-                    TransitionManager.beginDelayedTransition(binding.cflConstraint)
                     true
                 }
                 else -> false
@@ -83,5 +85,52 @@ class CFLFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // closes filter if it is open and user moves away from Overview page
+        if (filterShown) filterInteraction()
+    }
+
+    /**
+     *  Either opens or closes filter depending on filterShown.
+     */
+    private fun filterInteraction() {
+
+        // setting the new constraints for FilterFragment
+        constraints.connect(
+            R.id.fragment_filter_container, ConstraintSet.BOTTOM,
+            if (filterShown) R.id.cfl_constraint else R.id.cfl_filter_anchor, ConstraintSet.TOP, 0
+        )
+        // raises mask in order to appear above everything except ToolBar and FilterFragment
+        binding.cflFilterMask.elevation = 1f
+        binding.cflFilterMask.animate().apply {
+            interpolator = LinearInterpolator()
+            // if filter is shown then after animation lower mask elevation to be behind everything
+            setListener(if (filterShown) {
+                object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        binding.cflFilterMask.elevation = -1f
+                    }
+                }
+            } else {
+                null
+            })
+            duration = 400
+            // alpha change depending on if filter is shown
+            alpha(if (filterShown) 0f else .547f)
+            start()
+        }
+
+        // transition is only used to make filter animation last the same as mask alpha animation
+        val transition = AutoTransition()
+        transition.duration = 400
+
+        // apply constraints and start transition
+        constraints.applyTo(binding.cflConstraint)
+        TransitionManager.beginDelayedTransition(binding.cflConstraint, transition)
+        filterShown = !filterShown
     }
 }
